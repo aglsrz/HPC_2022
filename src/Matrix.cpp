8919d91957867cac486cd7976b3e20dde0019847
aglsrz <sirazovaaigul@gmail.com>
2022-12-03 12:13:15 +0300

    add cuda realisation

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -5,8 +5,11 @@ Matrix::Matrix() : size(0), values(nullptr) {}
 Matrix::Matrix(int sz)
 {
 	size = sz;
-	if (size <= 0)
-		throw exception("Invalid size of matrix.");
+	if (size <= 0){
+		std::cout << "Error: Creating matrix with size <= 0" << size << std::endl;
+		throw exception();
+	}
+		
 	values = new float* [size];
 	for (int i = 0; i < size; i++)
 		values[i] = new float[size];
@@ -15,8 +18,11 @@ Matrix::Matrix(int sz)
 Matrix::Matrix(int sz, float* plain)
 {
 	size = sz;
-	if (size <= 0)
-		throw exception("Invalid size of matrix.");
+	if (size <= 0){
+		std::cout << "Error: Creating matrix with size <= 0" << size << std::endl;
+		throw exception();
+	}
+		
 	values = new float* [size];
 	for (int i = 0; i < size; i++)
 		values[i] = new float[size];
@@ -51,18 +57,25 @@ int Matrix::getSize() const
 
 float Matrix::get(int i, int j) const
 {
-	if ((i > size) || (j > size))
-		throw exception("Invalid indexes.");
+	if ((i > size) || (j > size)){
+		std::cout << "Error: index out of boundaries" << std::endl;
+		throw exception();
+	}
 	return values[i][j];
 }
 
 void Matrix::readFromFile(ifstream& fptr)
 {
-	if (!fptr.is_open())
-		throw exception("Invalid ifstream.");
+	if (!fptr.is_open()){
+		std::cout << "Error: File not opened" << std::endl;
+		throw exception();
+	}
+		
 	fptr >> size;
-	if (size <= 0)
-		throw exception("Invalid size of matrix.");
+	if (size <= 0){
+		std::cout << "Error: size of file <=0" << std::endl;
+		throw exception();
+	}
 	values = new float* [size];
 	for (int i = 0; i < size; i++)
 		values[i] = new float[size];
@@ -72,22 +85,27 @@ void Matrix::readFromFile(ifstream& fptr)
 			if (!fptr.eof())
 				fptr >> values[i][j];
 			else {
-				throw exception("Failed reading matrix from file. End of file.");
+				std::cout << "Error: file is not fully read, but matrix full" << std::endl;
+				throw exception();
 			}
 }
 
 void Matrix::setValue(int i, int j, float val)
 {
-	if ((i > size) || (j > size))
-		throw exception("Invalid indexes.");
+	if ((i > size) || (j > size)){
+		std::cout << "Error:(set) index out of boundaries" << std::endl;
+		throw exception();
+	}
 	values[i][j] = val;
 }
 
 Matrix Matrix::convolution(const Matrix& kernel)
 {
 	int convSize = size - kernel.getSize() + 1;
-	if (convSize <= 0)
-		throw exception("Wrong matrix size for convolution.");
+	if (convSize <= 0){
+		std::cout << "Error: convSize <= 0" << std::endl;
+		throw exception();
+	}
 
 	Matrix convMatr(convSize);
 	float sum;
@@ -113,8 +131,10 @@ void Matrix::saveToFile(string filename, int dtime, int fsize)
 {
 	ofstream fout;
 	fout.open(filename);
-	if (!fout.is_open())
-		throw exception("File for writing is not opened.");
+	if (!fout.is_open()){
+		std::cout << "Error: no file to save matrix - " << filename << std::endl;
+		throw exception();
+	}
 
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j < size; ++j)
